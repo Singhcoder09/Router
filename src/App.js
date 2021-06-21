55a4779597c1cf1dce05b5f59c1b72da6a6a72bf
Gagandeep Singh <gagandeepssingh1992@gmail.com>
2021-06-20 22:25:46 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+export default function App() {
+
+  //Insert router, links here
+  
+  return(
+    <Router>
+      <h1 style={{ margin: 30, padding: 30}} >TV APPS</h1>
+
+      <div className="d-flex justify-content-center" style={{ margin: 30, padding: 30}}>
+        <Link to="/netflix" style={{ margin: 10, padding: 10 }}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/6/69/Netflix_logo.svg" alt="Netflix"/>
+        </Link>
+        
+        <Link to="/hbomax" style={{ margin: 10, padding: 10 }}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg" alt="HBOMax"/>
+        </Link>
+        
+        <Link to="/hulu" style={{ margin: 10, padding: 10 }}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Hulu_Logo.svg" alt="HULU"/>
+        </Link>
+        
+        <Link to="/prime" style={{ margin: 10, padding: 10 }}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Prime_Video.png" alt="PrimeVideo"/>
+        </Link>
+      </div>
+      
+      <Switch>
+        <Route exact path="/:id">
+          <Child/>
+        </Route>
+      </Switch>
+    
+    </Router>
   );
 }
 
-export default App;
+function Child() {
+
+  // Below this comment, there's one major key script missing
+
+  let params = useParams();
+  let id = params.id;
+  console.log(id);
+
+  return (
+    <div>
+      <h3>You Selected:<span>{id}</span></h3>
+    </div>
+  );
+}
